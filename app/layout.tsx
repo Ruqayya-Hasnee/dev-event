bc6e29be14aed9b722cac0579a1c434e1d82df1e
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-05-01 12:24:08 +0500

    basic ui setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,20 @@
+import LightRays from "@/components/LightRays";
+import { Martian_Mono, Schibsted_Grotesk, Geist } from "next/font/google";
 import "./globals.css";
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-sg",
+});
+const martianMono = Martian_Mono({
+  variable: "--font-mm",
+});
+
+export const metadata = {
+  title: "DevEvent",
+  description: "The best event for developers",
+};
 
 export default function RootLayout({
   children,
@@ -6,10 +22,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={`${geist.variable} ${schibstedGrotesk.variable} ${martianMono.variable}`}>
+        <div className="absolute inset-0 top-0 min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.2}
+            noiseAmount={0.0}
+            distortion={0.01}
+          />
+        </div>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
 const Page = () => {
   return (
-    <div>Wellcome to Nextjs 16</div>
-  )
+<section>
+  <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
+  <p className="text-center mt-5">
+    Hackathons, Meetups, and Conferences All in One Place
+  </p>
+</section> )
 }
 
 export default Page
